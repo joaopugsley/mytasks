c8b7ea2724b43f442c11ee65b815752f7711d119
joaopugsley <joaopugsleyy@gmail.com>
2024-01-16 20:22:04 -0300

    feat: updating username validators

--- backend/src/users/dto/CreateUserDTO.ts
+++ backend/src/users/dto/CreateUserDTO.ts
@@ -1,10 +1,18 @@
-import { IsEmail, IsString, Length } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  NotContains,
+} from 'class-validator';
 
 export class CreateUserDTO {
   @IsString({ message: 'Username must be a string' })
+  @IsNotEmpty({ message: 'Invalid Username' })
   @Length(4, 12, {
     message: 'Username length must be between 4 and 12 characters',
   })
+  @NotContains(' ', { message: 'Username cannot contains space' })
   username: string;
 
   @IsString({ message: 'Password must be a string' })
