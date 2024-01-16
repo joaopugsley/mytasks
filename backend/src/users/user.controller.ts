1ce599a79b5d6dfeb9e23f01ca35a23c8203278d
joaopugsley <joaopugsleyy@gmail.com>
2024-01-16 19:57:49 -0300

    feat: create POST '/api/users/activate/ route

--- backend/src/users/user.controller.ts
+++ backend/src/users/user.controller.ts
@@ -1,16 +1,22 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDTO } from './dto/CreateUserDTO';
+import { ActivateUserDTO } from './dto/ActivateUserDTO';
 
 @Controller('api/users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('/')
-  async createUser(
-    @Body(new ValidationPipe()) createUserDto: CreateUserDTO,
-  ): Promise<number> {
-    const user = await this.userService.createUser(createUserDto);
-    return user;
+  async createUser(@Body() data: CreateUserDTO): Promise<number> {
+    const result = await this.userService.createUser(data);
+    return result;
+  }
+
+  @Post('/activate')
+  @HttpCode(200)
+  async activateAccount(@Query() data: ActivateUserDTO) {
+    const result = await this.userService.activateUser(data);
+    return result;
   }
 }
