7cc5f1782f49c7e9a4c8c09240ec59ff9018c3f6
joaopugsley <joaopugsleyy@gmail.com>
2024-01-17 20:23:57 -0300

    feat: create 'CreateTaskDTO'

---
+++ backend/src/tasks/dto/CreateTaskDTO.ts
@@ -0,0 +1,15 @@
+import { IsString, Length, MaxLength } from 'class-validator';
+
+export class CreateTaskDTO {
+  @IsString({ message: 'Title must be a string' })
+  @Length(2, 30, {
+    message: 'Title length must be between 2 and 30 characters',
+  })
+  title: string;
+
+  @IsString({ message: 'Description must be a string' })
+  @MaxLength(255, {
+    message: 'Description length must be lower than 255 characters',
+  })
+  description: string;
+}
