de79138a5749ecd164ffd1fc1e6607011b1bf410
joaopugsley <joaopugsleyy@gmail.com>
2024-01-18 05:52:29 -0300

    feat: importing LoggerModule

--- backend/src/tasks/task.module.ts
+++ backend/src/tasks/task.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '@common/services/prisma/prisma.service';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { LoggerModule } from '@common/services/logger/logger.module';
 
 @Module({
+  imports: [LoggerModule],
   controllers: [TaskController],
   providers: [PrismaService, TaskService],
 })
