d48eb951306367ff6ed5d10f9e5ab8f56d40ea0a
joaopugsley <joaopugsleyy@gmail.com>
2024-01-18 05:51:17 -0300

    feat: importing 'MongooseModule' for root

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,14 @@
 import { CryptoModule } from '@common/services/crypto/crypto.module';
 import { EmailModule } from '@common/services/email/email.module';
-import { PrismaModule } from '@common/services/prisma/prisma.module';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { UserModule } from '@users/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TaskModule } from './tasks/task.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['.env.local'],
-    }),
-    PrismaModule,
+    MongooseModule.forRoot(process.env.MONGO_URL),
     CryptoModule,
     EmailModule,
     AuthModule,
