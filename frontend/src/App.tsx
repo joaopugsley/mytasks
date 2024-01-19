c54d3b801412f193d26da3b1f11ad5b340d6b284
joaopugsley <joaopugsleyy@gmail.com>
2024-01-19 19:31:15 -0300

    feat: login page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from './contexts/auth'
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage/>}/>
           <Route path="/register" element={<RegisterPage/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
         </Routes>
       </AuthProvider>
     </BrowserRouter>
