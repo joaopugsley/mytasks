3cabe576e636278f7f2b34bdda368ccbb2f64fae
joaopugsley <joaopugsleyy@gmail.com>
2024-01-21 00:17:32 -0300

    feat: 'checkAuth' function + rightly setting authorization at api default header

--- frontend/src/contexts/auth.tsx
+++ frontend/src/contexts/auth.tsx
@@ -1,9 +1,9 @@
-import { createContext, ReactNode, useContext, useEffect, useState } from "react";
-import { api } from "../services/api";
-import axios from "axios";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../lib/api";
 import Cookies from "js-cookie";
 
 type AuthContextData = {
+  checkAuth: () => void;
   signIn: (username: string, password: string) => Promise<void>;
   signOut: () => void;
   isAuthenticated: boolean;
@@ -18,13 +18,15 @@ export const AuthContext = createContext({} as AuthContextData)
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [isAuthenticated, setAuthenticated] = useState<boolean>(false);
 
-  useEffect(() => {
+  const checkAuth = () => {
     const access_token = Cookies.get('access_token');
     if (access_token) {
-      axios.defaults.headers.common['Authorization'] = `Bearer ${access_token}`;
+      api.defaults.headers.common['Authorization'] = `Bearer ${access_token}`;
       setAuthenticated(true);
+    } else {
+      setAuthenticated(false);
     }
-  }, []);
+  };
 
   const signIn = async (username: string, password: string) => {
     try {
@@ -36,7 +38,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       const { access_token } = response.data;
 
       // set the authorization header for subsequent requests
-      axios.defaults.headers.common['Authorization'] = `Bearer ${access_token}`;
+      api.defaults.headers.common['Authorization'] = `Bearer ${access_token}`;
       
       // calculate expire date
       const expires = new Date(new Date().getTime() + 120 * 60 * 1000); // 120 minutes = 2 hours
@@ -59,13 +61,13 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     setAuthenticated(false);
   }
 
+  useEffect(() => {
+    checkAuth()
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ signIn, signOut, isAuthenticated }}>
+    <AuthContext.Provider value={{ checkAuth, signIn, signOut, isAuthenticated }}>
       {children}
     </AuthContext.Provider>
   );
 }
-
-export const useAuth = () => {
-  return useContext(AuthContext);
-};
