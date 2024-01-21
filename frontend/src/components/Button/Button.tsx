435ee74ef21a4caad7ef8aacef168cd0e2eb8465
joaopugsley <joaopugsleyy@gmail.com>
2024-01-21 00:16:07 -0300

    feat: 'onClick' prop on Button component

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -6,11 +6,12 @@ export type ButtonProps = {
   className?: string;
   children?: ReactNode;
   disabled?: boolean;
+  onClick?: () => void;
 }
 
-export const Button = ({ type = 'button', children, disabled = false, className }: ButtonProps) => {
+export const Button = ({ type = 'button', children, disabled = false, className, onClick }: ButtonProps) => {
   return (
-    <button type={type} disabled={disabled} className={twMerge("w-full py-3 mt-3 bg-black text-white rounded-md shadow-md transition duration-300 hover:bg-gray-900", className || '')}>
+    <button type={type} onClick={onClick} disabled={disabled} className={twMerge("w-full py-3 bg-black text-white rounded-md shadow-md transition duration-300 hover:bg-gray-900", className || '')}>
       {children}
     </button>
   )
