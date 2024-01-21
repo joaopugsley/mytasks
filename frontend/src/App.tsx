caf1b2f49aaccf15d1d0fd9bebf9f9911af43f64
joaopugsley <joaopugsleyy@gmail.com>
2024-01-21 00:22:35 -0300

    feat: handling new pages on router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { AuthProvider } from './contexts/auth'
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import TasksPage from "./pages/TasksPage";
+import ActivationPage from "./pages/ActivationPage";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path="/" element={<HomePage/>}/>
           <Route path="/register" element={<RegisterPage/>}/>
           <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/tasks" element={<TasksPage/>}/>
+          <Route path="/account/activate/:key" element={<ActivationPage/>}/>
         </Routes>
       </AuthProvider>
     </BrowserRouter>
